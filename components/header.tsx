91dce064907b436b173fb75b5fc994ce7b14e221
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-08-08 11:25:28 +0300

    feat: populate the avatar with user information

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Navbar, NavbarContent } from '@nextui-org/navbar'
+import { useState, useEffect } from 'react'
+import { createClient } from '@/utils/supabase/client'
 import { Avatar } from '@nextui-org/avatar'
 import {
   Dropdown,
@@ -9,33 +10,47 @@ import {
   DropdownTrigger
 } from '@nextui-org/dropdown'
 
+import type { User } from '@supabase/supabase-js'
+
 const Header = () => {
+  const [user, setUser] = useState<User | null>(null)
+  const supabase = createClient()
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser()
+      if (data.user) setUser(data.user)
+    }
+
+    getUser()
+  }, [])
+
   return (
-    <header className="w-full h-12 bg-red-500 flex items-center px-4">
+    <header className="w-full h-12 flex items-center px-4">
       <Dropdown placement="bottom-end">
         <DropdownTrigger>
           <Avatar
             isBordered
             as="button"
             className="transition-transform"
-            color="secondary"
-            name="Jason Hughes"
+            color="primary"
+            name={user?.user_metadata.name}
             size="sm"
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            src={user?.user_metadata.picture}
           />
         </DropdownTrigger>
         <DropdownMenu aria-label="Profile Actions" variant="flat">
           <DropdownItem key="profile" className="h-14 gap-2">
-            <p className="font-semibold">Signed in as</p>
-            <p className="font-semibold">zoey@example.com</p>
+            <p className="font-semibold">{user?.user_metadata.name}</p>
           </DropdownItem>
-          <DropdownItem key="settings">My Settings</DropdownItem>
-          <DropdownItem key="team_settings">Team Settings</DropdownItem>
-          <DropdownItem key="analytics">Analytics</DropdownItem>
-          <DropdownItem key="system">System</DropdownItem>
-          <DropdownItem key="configurations">Configurations</DropdownItem>
+          <DropdownItem key="settings">Settings</DropdownItem>
           <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-          <DropdownItem key="logout" color="danger">
+          <DropdownItem key="plugins">Plugins</DropdownItem>
+          <DropdownItem
+            key="logout"
+            color="danger"
+            onClick={async () => await supabase.auth.signOut()}
+          >
             Log Out
           </DropdownItem>
         </DropdownMenu>

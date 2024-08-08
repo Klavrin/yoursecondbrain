c295a508fadc78432c2783db73ced3dcfe3feec8
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-08-08 18:52:44 +0300

    feat: add sidebar dropdown-menu

---
+++ components/sidebar-dropdown-menu.tsx
@@ -0,0 +1,103 @@
+import { useUserStore } from '@/store/user-store'
+import { Avatar } from '@nextui-org/avatar'
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger
+} from '@nextui-org/dropdown'
+import React, { ReactNode } from 'react'
+
+interface SidebarDropdownMenuProps {
+  children: ReactNode
+}
+
+const SidebarDropdownMenu: React.FC<SidebarDropdownMenuProps> = ({ children }) => {
+  const user = useUserStore((state) => state.user)
+
+  return (
+    <Dropdown
+      showArrow
+      radius="sm"
+      classNames={{
+        base: 'before:bg-default-200', // change arrow background
+        content: 'p-0 border-small border-divider bg-background'
+      }}
+      className="w-72"
+    >
+      <DropdownTrigger>{children}</DropdownTrigger>
+      <DropdownMenu
+        aria-label="Custom item styles"
+        disabledKeys={['profile']}
+        className="p-3"
+        itemClasses={{
+          base: [
+            'rounded-md',
+            'text-default-500',
+            'transition-opacity',
+            'data-[hover=true]:text-foreground',
+            'data-[hover=true]:bg-default-100',
+            'dark:data-[hover=true]:bg-default-50',
+            'data-[selectable=true]:focus:bg-default-50',
+            'data-[pressed=true]:opacity-70',
+            'data-[focus-visible=true]:ring-default-500'
+          ]
+        }}
+      >
+        <DropdownSection aria-label="Profile & Actions" showDivider>
+          <DropdownItem isReadOnly key="profile" className="h-14 gap-2">
+            <div className="flex items-center gap-2">
+              <Avatar src={user?.user_metadata.picture} className="w-8 h-8" />
+              <div>
+                <h5 className="text-small text-neutral-800 font-semibold">
+                  {user?.user_metadata.name}
+                </h5>
+                <p className="text-[12px] text-neutral-500">{user?.email}</p>
+              </div>
+            </div>
+          </DropdownItem>
+          <DropdownItem key="dashboard">Dashboard</DropdownItem>
+          <DropdownItem key="settings">Settings</DropdownItem>
+          <DropdownItem
+            key="new_project"
+            // endContent={<PlusIcon className="text-large" />}
+          >
+            New Project
+          </DropdownItem>
+        </DropdownSection>
+
+        <DropdownSection aria-label="Preferences" showDivider>
+          <DropdownItem key="quick_search" shortcut="⌘K">
+            Quick search
+          </DropdownItem>
+          <DropdownItem
+            isReadOnly
+            key="theme"
+            className="cursor-default"
+            endContent={
+              <select
+                className="z-10 outline-none w-16 py-0.5 rounded-md text-tiny group-data-[hover=true]:border-default-500 border-small border-default-300 dark:border-default-200 bg-transparent text-default-500"
+                id="theme"
+                name="theme"
+              >
+                <option>System</option>
+                <option>Dark</option>
+                <option>Light</option>
+              </select>
+            }
+          >
+            Theme
+          </DropdownItem>
+        </DropdownSection>
+
+        <DropdownSection aria-label="Help & Feedback">
+          <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+          <DropdownItem key="logout">Log Out</DropdownItem>
+        </DropdownSection>
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+export default SidebarDropdownMenu
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,14 +4,12 @@ import { Avatar } from '@nextui-org/avatar'
 import { Button } from '@nextui-org/button'
 import { Tooltip } from '@nextui-org/tooltip'
 import { FaAnglesLeft } from 'react-icons/fa6'
-import { twMerge } from 'tailwind-merge'
+import SidebarDropdownMenu from './sidebar-dropdown-menu'
 
 const Sidebar = () => {
   const user = useUserStore((state) => state.user)
   const sidebarOpened = useSidebarStore((state) => state.sidebarOpened)
   const setSidebarValue = useSidebarStore((state) => state.setSidebarValue)
-
-  console.log(sidebarOpened)
 
   return (
     <div
@@ -19,25 +17,31 @@ const Sidebar = () => {
       style={{ display: sidebarOpened ? 'block' : 'none' }}
     >
       <div className="h-14 flex items-center">
-        <div className="w-full p-2 rounded-lg hover:bg-neutral-200 active:bg-neutral-300 transition-colors duration-150 flex items-center gap-2 h-[40px]">
-          <Avatar src={user?.user_metadata.picture} className="w-8 h-8" />
-          <div>
-            <h5 className="text-small text-neutral-800 font-semibold">
-              {user?.user_metadata.name}
-            </h5>
-            <p className="text-[12px] text-neutral-500">Premium</p>
+        <SidebarDropdownMenu>
+          <div
+            className="w-full p-2 rounded-lg hover:bg-neutral-200 active:bg-neutral-300 transition-colors duration-150 flex items-center gap-2 h-[40px]"
+            onClick={() => console.log('show popover')}
+          >
+            <Avatar src={user?.user_metadata.picture} className="w-8 h-8" />
+            <div>
+              <h5 className="text-small text-neutral-800 font-semibold">
+                {user?.user_metadata.name}
+              </h5>
+              <p className="text-[12px] text-neutral-500">Premium</p>
+            </div>
+
+            <Tooltip showArrow content="Close sidebar" size="sm">
+              <Button
+                size="sm"
+                variant="light"
+                className="min-h-[32px] min-w-[38px] ml-auto"
+                onClick={() => setSidebarValue(false)}
+              >
+                <FaAnglesLeft size={14} />
+              </Button>
+            </Tooltip>
           </div>
-          <Tooltip showArrow content="Close sidebar" size="sm">
-            <Button
-              size="sm"
-              variant="light"
-              className="min-h-[32px] min-w-[38px] ml-auto"
-              onClick={() => setSidebarValue(false)}
-            >
-              <FaAnglesLeft size={14} />
-            </Button>
-          </Tooltip>
-        </div>
+        </SidebarDropdownMenu>
       </div>
     </div>
   )

cec6e5f0c3bad16c09be42c18927a3d31b53540f
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-08-12 21:04:13 +0300

    feat: insert new tasks into database & use supabase realtime to reflect the changes to the user

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import Header from '@/components/header'
-import Sidebar from '@/components/sidebar'
+import { useEffect, useState } from 'react'
+import { createClient } from '@/utils/supabase/client'
 import { Button } from '@nextui-org/button'
 import { Checkbox } from '@nextui-org/checkbox'
 import {
@@ -12,8 +12,41 @@ import {
   TableHeader,
   TableRow
 } from '@nextui-org/table'
+import { Input } from '@nextui-org/input'
+import Header from '@/components/header'
+import Sidebar from '@/components/sidebar'
+import { useUser } from '@/provider/user-provider'
+import { formatTime } from '@/utils/format-time'
+import { useTasksSubscription } from '@/hooks/use-tasks-subscription'
+
+export enum TaskStatus {
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED'
+}
 
 const Tasks = () => {
+  // const [tasks, setTasks] = useState<any>([])
+  const [taskName, setTaskName] = useState('')
+  const supabase = createClient()
+  const { user } = useUser()
+  const tasks = useTasksSubscription()
+
+  const handleCreateTask = async (
+    task: string,
+    status: TaskStatus,
+    userId: string,
+    isCompleted: boolean
+  ) => {
+    const { error } = await supabase.from('tasks').insert({
+      task,
+      status,
+      user_id: userId,
+      is_completed: isCompleted
+    })
+
+    if (error) throw error
+  }
+
   return (
     <div className="flex">
       <Sidebar />
@@ -21,7 +54,18 @@ const Tasks = () => {
         <Header />
         <div className="flex flex-col items-center">
           <div className="w-full px-2 max-w-[1028px] mx-auto mt-4">
-            <Button className="w-full mb-4" color="primary">
+            <Input
+              placeholder="Task name..."
+              onChange={(e) => setTaskName(e.target.value)}
+              value={taskName}
+            />
+            <Button
+              onClick={() =>
+                handleCreateTask(taskName, TaskStatus.IN_PROGRESS, user.id, false)
+              }
+              className="w-full mb-4 mt-1"
+              color="primary"
+            >
               Add new task
             </Button>
 
@@ -33,14 +77,16 @@ const Tasks = () => {
                 <TableColumn>STATUS</TableColumn>
               </TableHeader>
               <TableBody>
-                <TableRow key="1">
-                  <TableCell>
-                    <Checkbox />
-                  </TableCell>
-                  <TableCell>This is the content of the task</TableCell>
-                  <TableCell>12.08.2024</TableCell>
-                  <TableCell>Doing</TableCell>
-                </TableRow>
+                {tasks.map((task: any) => (
+                  <TableRow key={task.id}>
+                    <TableCell>
+                      <Checkbox value={task.is_completed} />
+                    </TableCell>
+                    <TableCell>{task.task}</TableCell>
+                    <TableCell>{formatTime(task.created_at)}</TableCell>
+                    <TableCell>{task.status}</TableCell>
+                  </TableRow>
+                ))}
               </TableBody>
             </Table>
           </div>

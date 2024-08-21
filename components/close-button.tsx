13b0471bb4ea65e77acac40f7fb77f30733b5d37
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2024-08-21 20:39:49 +0300

    feat: add close button

---
+++ components/close-button.tsx
@@ -0,0 +1,33 @@
+interface CloseButtonProps {
+  onClick?: () => void
+}
+
+const CloseButton: React.FC<CloseButtonProps> = ({ onClick }) => {
+  return (
+    <button
+      role="button"
+      aria-label="Close"
+      className="appearance-none select-none top-1 right-1 rtl:left-1 rtl:right-[unset] p-2 text-foreground-500 rounded-full hover:bg-default-100 active:bg-default-200 tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2"
+      type="button"
+      onClick={onClick}
+    >
+      <svg
+        aria-hidden="true"
+        fill="none"
+        focusable="false"
+        height="1em"
+        role="presentation"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        width="1em"
+      >
+        <path d="M18 6L6 18M6 6l12 12"></path>
+      </svg>
+    </button>
+  )
+}
+
+export default CloseButton
